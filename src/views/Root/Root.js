b0738adf7e14e4434d46e9baf515d5a3535c00a9
Mateusz Wysocki <mateusz.wysocki53@gmail.com>
2019-10-27 20:40:29 +0100

    configured absolute paths for project and made structure

--- src/views/Root/Root.js
+++ src/views/Root/Root.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {BrowserRouter} from "react-router-dom";
-import Sidebar from '../components/organisms/Sidebar';
+import Sidebar from 'components/organisms/Sidebar';
 
 const Root = ()=>(
   <BrowserRouter>
